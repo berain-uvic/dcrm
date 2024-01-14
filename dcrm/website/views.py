5bcb0fe613a1fba274ce9eabe7b9cd3cbdd9c993
Bryan Eriksson <bryaneriksson@gmail.com>
2024-01-14 15:05:15 -0800

    app created

---
+++ dcrm/website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
